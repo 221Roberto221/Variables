ccf35cd25f81caff42bdad8e52ebc25c22a701c9
Roberto Andrade Salazar <andrade.roberto@uabc.edu.mx>
2022-02-23 13:30:32 -0800

    Se agrego el codigo para sacar el area de un triangulo (Reto)

---
+++ CalculoArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vala = 10;
+            int valb = 10;
+            int valc = 7;
+            float area= (vala*valb)/2;
+            Console.WriteLine("El AREA del triangulo es: " + area);
+            Console.WriteLine("Presiona cualquier tecla para finalizar ");
+            Console.ReadKey();
+        }
+    }
+}
