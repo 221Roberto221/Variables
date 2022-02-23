48d40b5b3e0d196b2cd6ae1915d91078e7365997
rober <rober@DESKTOP-PAQ4LE4>
2022-02-22 18:53:28 -0800

    Agregar archivos de proyecto.

---
+++ Enteros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enteros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int num3 = 20;
+            int total = num1 + num2 + num3;
+            Console.WriteLine ("El valor de SUMA es: " + total);
+            Console.WriteLine("Presiona cualquier tecla para continuar ");
+            Console.ReadKey();
+
+        }
+    }
+}
