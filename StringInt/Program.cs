9d978d573e1a3d4f3e600389dd0653f14518c116
Roberto Andrade Salazar <andrade.roberto@uabc.edu.mx>
2022-02-24 22:18:37 -0800

    Se agrego ejercicio "String" y reto "StringInt"

---
+++ StringInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            Console.WriteLine("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Te llamas " + nombre + " y tienes: " + edad + " años.");
+            Console.WriteLine("Presione cualquier tecla para finalizar ");
+            Console.ReadKey();
+
+        }
+    }
+}
